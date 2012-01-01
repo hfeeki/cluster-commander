a5b5118ef21ba7fcc9237d4b715f474d00ecd58e
Siraaj Khandkar <siraaj@khandkar.net>
2012-01-01 01:08:04 -0500

    Refactoring: got rid of _options module after all...

--- src/commander.erl
+++ src/commander.erl
@@ -15,16 +15,23 @@
 -include("commander_types.hrl").
 
 
+%%%============================================================================
+%%% API
+%%%============================================================================
+
 %%-----------------------------------------------------------------------------
 %% Function : main/1
 %% Purpose  : Entry point. Gets options, a list of nodes and spawns workers.
 %% Type     : none()
 %%-----------------------------------------------------------------------------
+main([]) ->
+    usage();
+
 main(Args) ->
     %
     % Get options
     %
-    Options = commander_options:get_options(Args),
+    Options = get_options_or_usage(Args),
 
     %
     % Pack job data
@@ -77,3 +84,43 @@ main(Args) ->
     % Global timeout
     %
     timer:sleep(Options#options.global_timeout).
+
+
+%%%============================================================================
+%%% Internal
+%%%============================================================================
+
+%%-----------------------------------------------------------------------------
+%% Function : get_options_or_usage/1
+%% Purpose  : Parses and packs CLI options and arguments into #options{} record.
+%% Type     : #options{}
+%%-----------------------------------------------------------------------------
+get_options_or_usage(Args) ->
+    case getopt:parse(?OPT_SPECS, Args) of
+        {ok, {OptList, CommandsList}} ->
+            #options{
+                user = proplists:get_value(user, OptList),
+                ssh_provider = proplists:get_value(ssh_provider, OptList),
+                host_timeout = proplists:get_value(host_timeout, OptList),
+                global_timeout =
+                    case proplists:get_value(global_timeout, OptList) of
+                        0 -> infinity;
+                        OtherGlobalTimeout -> OtherGlobalTimeout * 1000
+                    end,
+                port = proplists:get_value(port, OptList),
+                try_all_nodes = proplists:get_value(try_all_nodes, OptList),
+                command = string:join(CommandsList, " ")
+            };
+
+        {error, _} -> usage()
+    end.
+
+
+%%-----------------------------------------------------------------------------
+%% Function : usage/0
+%% Purpose  : Prints usage instructions and halts BEAM.
+%% Type     : none()
+%%-----------------------------------------------------------------------------
+usage() ->
+    getopt:usage(?OPT_SPECS, ?MODULE, "command"),
+    halt(1).
