1d12adb2add783fc627a0b5a58256619041bb5f1
Siraaj Khandkar <siraaj@khandkar.net>
2012-03-07 12:47:59 -0500

    Refactoring: pushing options-packing out of main function.

--- src/commander.erl
+++ src/commander.erl
@@ -26,37 +26,7 @@
 %%-----------------------------------------------------------------------------
 main(Args            ) when is_list(Args) -> main(get_options(Args));
 main({error, Reason })                    -> usage(Reason);
-main({ok,    Options})                    ->
-    Operation     = Options#options.operation,
-    GlobalTimeout = Options#options.global_timeout,
-
-    % Temporary work-around until I implement OTP-backed transport (SCP)
-    SSHProvider = case commander_lib:lookup_operation_type(Operation) of
-        transport -> os;
-        execution -> Options#options.ssh_provider
-    end,
-
-    WorkerModule = join_atoms([?MODULE, worker, SSHProvider], "_"),
-
-    % Pack nodes options
-    NodesOpts = #nodes_opts{
-        nodes         = Options#options.nodes,
-        nodes_group   = Options#options.nodes_group,
-        try_all_nodes = Options#options.try_all_nodes
-    },
-
-    % Pack job options
-    Job = #job{
-        operation    = Operation,
-        user         = Options#options.user,
-        command      = Options#options.command,
-        save_data_to = Options#options.save_data_to,
-        timeout      = Options#options.host_timeout,
-        port         = Options#options.port,
-        path_from    = Options#options.path_from,
-        path_to      = Options#options.path_to
-    },
-
+main({ok,    {SSHProvider, WorkerModule, Job, NodesOpts, GlobalTimeout}}) ->
     % Get a list of target nodes
     case commander_nodes:get_nodes(NodesOpts) of
         {error, Reason} ->
@@ -199,29 +169,59 @@ get_options(Args) ->
 %% Type     : #options{}
 %%-----------------------------------------------------------------------------
 get_packed_options(OptList, Operation, Commands, Paths) ->
-    {ok,
-        #options{
-            operation      = Operation,
-            command        = string:join(Commands, " "),
+    Options = #options{
+        operation      = Operation,
+        command        = string:join(Commands, " "),
 
-            path_from      = proplists:get_value(from,           Paths),
-            path_to        = proplists:get_value(to,             Paths),
+        path_from      = proplists:get_value(from,           Paths),
+        path_to        = proplists:get_value(to,             Paths),
 
-            user           = proplists:get_value(user,           OptList),
-            nodes          = proplists:get_value(nodes,          OptList),
-            nodes_group    = proplists:get_value(nodes_group,    OptList),
-            ssh_provider   = proplists:get_value(ssh_provider,   OptList),
-            port           = proplists:get_value(port,           OptList),
-            try_all_nodes  = proplists:get_value(try_all_nodes,  OptList),
-            save_data_to   = proplists:get_value(save_data_to,   OptList),
-            host_timeout   = proplists:get_value(host_timeout,   OptList),
-            global_timeout =
-                        case proplists:get_value(global_timeout, OptList) of
-                            0     -> infinity;
-                            Other -> Other * 1000
-                        end
-        }
-    }.
+        user           = proplists:get_value(user,           OptList),
+        nodes          = proplists:get_value(nodes,          OptList),
+        nodes_group    = proplists:get_value(nodes_group,    OptList),
+        ssh_provider   = proplists:get_value(ssh_provider,   OptList),
+        port           = proplists:get_value(port,           OptList),
+        try_all_nodes  = proplists:get_value(try_all_nodes,  OptList),
+        save_data_to   = proplists:get_value(save_data_to,   OptList),
+        host_timeout   = proplists:get_value(host_timeout,   OptList),
+        global_timeout =
+                    case proplists:get_value(global_timeout, OptList) of
+                        0     -> infinity;
+                        Other -> Other * 1000
+                    end
+    },
+
+    Operation     = Options#options.operation,
+    GlobalTimeout = Options#options.global_timeout,
+
+    % Temporary work-around until I implement OTP-backed transport (SCP)
+    SSHProvider = case commander_lib:lookup_operation_type(Operation) of
+        transport -> os;
+        execution -> Options#options.ssh_provider
+    end,
+
+    WorkerModule = join_atoms([?MODULE, worker, SSHProvider], "_"),
+
+    % Pack nodes options
+    NodesOpts = #nodes_opts{
+        nodes         = Options#options.nodes,
+        nodes_group   = Options#options.nodes_group,
+        try_all_nodes = Options#options.try_all_nodes
+    },
+
+    % Pack job options
+    Job = #job{
+        operation    = Operation,
+        user         = Options#options.user,
+        command      = Options#options.command,
+        save_data_to = Options#options.save_data_to,
+        timeout      = Options#options.host_timeout,
+        port         = Options#options.port,
+        path_from    = Options#options.path_from,
+        path_to      = Options#options.path_to
+    },
+
+    {ok, {SSHProvider, WorkerModule, Job, NodesOpts, GlobalTimeout}}.
 
 
 %%-----------------------------------------------------------------------------
